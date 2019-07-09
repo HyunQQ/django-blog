75ea5d47c20593828dd9d7b3cc9bf5d232557ca8
hyunq <5hyung@datasolution.kr>
2019-07-10 08:59:06 +0900

    화면 이동 장애 해결

--- blog/urls.py
+++ blog/urls.py
@@ -14,6 +14,4 @@ urlpatterns=[
     path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
     path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
     path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
-    # url(r'^search/$',views.post_search, name='post_search'),
-     # url(r'^post/(?P<pk>\d+)/comment/$', views.PostDetail.add_comment_to_post, name='add_comment_to_post'),
 ]
