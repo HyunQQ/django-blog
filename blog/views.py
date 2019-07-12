1d5f41f9fcc76fadac91cf740ae9cdb42c4ec1ed
hyunq <qqkqqk23@naver.com>
2019-07-12 22:55:30 +0900

    redirect url error 해결

--- blog/views.py
+++ blog/views.py
@@ -61,7 +61,7 @@ def post_new(request):
             # post.published_date = timezone.now()
             post.save()
 
-            return redirect('post_detail', post.pk)
+            return redirect('blog:post_detail', post.pk)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form':form})
@@ -78,7 +78,7 @@ def post_edit(request, pk):
             post.author = request.user
             # post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk = post.pk)
+            return redirect('blog:post_detail', pk = post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html',{'form':form})
@@ -90,7 +90,7 @@ def post_draft_list(request):
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
-    return redirect('post_detail', pk=pk)
+    return redirect('blog:post_detail', pk=pk)
 
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -105,7 +105,7 @@ def add_comment_to_post(request, pk):
             comment = form.save(commit=False)
             comment.post = post
             comment.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect('blog:post_detail', pk=post.pk)
     else:
         form = CommentForm()
     return render(request, 'blog/add_comment_to_post.html',{'form':form})
@@ -114,13 +114,13 @@ def add_comment_to_post(request, pk):
 def comment_approve(request, pk):
     comment = get_object_or_404(Comment,pk=pk)
     comment.approve()
-    return redirect('post_detail', pk=comment.post.pk)
+    return redirect('blog:post_detail', pk=comment.post.pk)
 
 @login_required
 def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.delete()
-    return redirect('post_detail',pk=comment.post.pk)
+    return redirect('blog:post_detail',pk=comment.post.pk)
 
     
     
