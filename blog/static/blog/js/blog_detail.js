2c53c683a89b773651d1f10076f63aaa8e64331c
Hyungyu <qqkqqk23@naver.com>
2019-07-23 15:11:46 +0900

    detail page html tag 인식 수정

--- blog/static/blog/js/blog_detail.js
+++ blog/static/blog/js/blog_detail.js
@@ -1,4 +1,11 @@
+
+
 $(document).ready(function () {
     $(".edit_comment textarea").attr('placeholder','댓글 입력');
     $(".edit_comment input").attr('placeholder','사용자 이름');
-});
+    
+    var test = htmlDecode(post_text);
+    $("#post_text").append(test);
+}); 
+
+
